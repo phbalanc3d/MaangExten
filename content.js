0f0eb535b248849d8ebef147223261c8d28ed43a
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-06-03 01:24:16 +0530

    added bookmark button on the webpage

--- content.js
+++ content.js
@@ -1,7 +1,22 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png");
+
+window.addEventListener("load", addBookmarkButton);
+
+function addBookmarkButton() {
+    const bookmarkButton = document.createElement("img");
+    bookmarkButton.id = "add-bookmark-button";
+    bookmarkButton.src = bookmarkImgURL;
+
+    bookmarkButton.style.width = "24px";
+    bookmarkButton.style.height = "24px";
+    bookmarkButton.style.cursor = "pointer";
+    const header = document.querySelector(
+  ".flex.flex-wrap.items-start.justify-between.gap-3"
+);
+
+header.style.display = "flex";
+header.style.justifyContent = "space-between";
+header.style.alignItems = "center";
+console.log(header);
+header.appendChild(bookmarkButton);
+}
